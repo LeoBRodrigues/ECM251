2e2627384a3c7e6ccd1eb1a6e876624d0706647d
unknown <leonardo_r@fastmail.com>
2020-04-06 10:25:28 -0300

    segundo commit

---
+++ atividades/atividade1/Conta.java
@@ -0,0 +1,10 @@
+
+public class Conta {
+
+    private double saldoInicial;
+
+    public Conta(double saldoInicial) 
+    {
+        this.saldoInicial = saldoInicial;
+    }
+}
---
+++ atividades/atividade1/Usuario.java
@@ -0,0 +1,15 @@
+
+public class Usuario {
+
+    private String name;
+    private String senha;
+    private String email;
+
+    public Usuario(String name, String senha, String email)
+    {
+        this.name = name;
+        private this.senha = senha;
+        this.email = email
+    }
+
+}
